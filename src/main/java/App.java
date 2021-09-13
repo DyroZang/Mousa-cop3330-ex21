0aaf4f8aed802101e0ee8cd177c1967a1120257f
Gabriel Mousa <gab.01@hotmail.com>
2021-09-13 10:57:55 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,58 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 21 Solution
+ *  Copyright 2021 Gabriel Mousa
+ */
+
+import java.util.Scanner;
+
+public class App{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String month = "";
+
+
+        System.out.println("Please enter the number of the month: ");
+        int num = Integer.parseInt(input.nextLine());
+
+        switch(num){
+            case 1:
+                month = "January";
+                break;
+            case 2:
+                month = "February";
+                break;
+            case 3:
+                month = "March";
+                break;
+            case 4:
+                month = "April";
+                break;
+            case 5:
+                month = "May";
+                break;
+            case 6:
+                month = "June";
+                break;
+            case 7:
+                month = "July";
+                break;
+            case 8:
+                month = "August";
+                break;
+            case 9:
+                month = "September";
+                break;
+            case 10:
+                month = "October";
+                break;
+            case 11:
+                month = "November";
+                break;
+            case 12:
+                month = "December";
+                break;
+        }
+
+        System.out.format("The name of the month is %s.", month);
+    }
+}
